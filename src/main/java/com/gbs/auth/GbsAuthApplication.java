1e772fdacb1163a64d5244ed91a15638ac2e9576
jay-inexture <jayjiyani@inexture.com>
2024-03-18 10:57:25 +0530

    dev: init

---
+++ src/main/java/com/gbs/auth/GbsAuthApplication.java
@@ -0,0 +1,13 @@
+package com.gbs.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbsAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GbsAuthApplication.class, args);
+    }
+
+}
